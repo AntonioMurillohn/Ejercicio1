d8d5672ba7426d8edf71a516b37a6e0e47e15b31
AntonioMurillohn <136047301+AntonioMurillohn@users.noreply.github.com>
2023-10-09 22:37:00 -0600

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Ejercicio1._1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+ 
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿namespace Ejercicio1._1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Sumar_Clicked(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(numero1.Text);
+            double num2 = Convert.ToDouble(numero2.Text);
+            double resultado = num1 + num2;
+            resultadoLabel.Text = "Resultado: " + resultado.ToString();
+        }
+
+        private void Restar_Clicked(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(numero1.Text);
+            double num2 = Convert.ToDouble(numero2.Text);
+            double resultado = num1 - num2;
+            resultadoLabel.Text = "Resultado: " + resultado.ToString();
+        }
+
+        private void Multiplicar_Clicked(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(numero1.Text);
+            double num2 = Convert.ToDouble(numero2.Text);
+            double resultado = num1 * num2;
+            resultadoLabel.Text = "Resultado: " + resultado.ToString();
+        }
+
+        private void Dividir_Clicked(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(numero1.Text);
+            double num2 = Convert.ToDouble(numero2.Text);
+
+            if (num2 != 0)
+            {
+                double resultado = num1 / num2;
+                resultadoLabel.Text = "Resultado: " + resultado.ToString();
+            }
+            else
+            {
+                resultadoLabel.Text = "No se puede dividir por cero.";
+            }
+        }
+    }
+}
